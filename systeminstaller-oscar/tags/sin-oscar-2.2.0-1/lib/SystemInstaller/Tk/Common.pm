7f96780ed58eaa747c16e432fad4373b05b293ba
efocht <efocht@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-04-11 22:08:54 +0000

    Tag for version 2.2.0-1 of sin-oscar.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@4548 18f9d06c-6ae2-0310-8438-938a554feff9

---
+++ systeminstaller-oscar/tags/sin-oscar-2.2.0-1/lib/SystemInstaller/Tk/Common.pm
@@ -0,0 +1,248 @@
+package SystemInstaller::Tk::Common;
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+use base qw(Exporter);
+use vars qw(@EXPORT %Labels);
+use AppConfig;
+use File::Basename;
+use strict;
+
+@EXPORT = qw(selector2entry
+			 quit_button
+			 label_entry_line 
+			 label_option_line 
+			 label_listbox_line 
+			 resethash
+			 imageexists
+			 init_si_config
+			 center_window
+			 yn_window
+			 done_window
+			 error_window
+			);
+
+%Labels = ();
+
+#
+#  selector2entry sets up the callback needed for a fileselector to be tied to 
+#  an entry box
+#  
+
+sub selector2entry {
+	my ($var, $title, $filter, $widget) = @_;
+
+	# now we attempt to do some reasonable directory setting
+	my $dir = $$var;
+	$dir = dirname( $dir ) unless -d $dir;
+	my $t = $widget->getOpenFile(
+		-title => $title,
+		-initialdir => $dir,
+		-filetypes => $filter,
+	);
+	$$var = $t if $t && -e $t;
+}
+
+sub reset_window {
+	my ($window, $curvars, $defvars, $optiondefaults) = @_;
+	resethash($curvars, $defvars);
+	foreach my $key (keys %$optiondefaults) {
+		$$optiondefaults{$key}->setOption($$curvars{$key}) if($$optiondefaults{$key});
+	}
+}
+
+sub close_after {
+	my ($window, $onclose, @args) = @_;
+	&$onclose(@args) if(ref($onclose) eq "CODE");
+	$window->destroy;
+}
+
+# Just the standard Tk window centering code...
+#
+sub center_window {
+	my $w = shift;
+	my $p = $w->parent();
+
+	$w->withdraw() if $w->viewable();
+
+	$w->idletasks;
+	my $x = int( ($w->screenwidth - $w->reqwidth)/2 );
+	my $y = int( ($w->screenheight - $w->reqheight)/2 );
+	if( $p ) {
+		$x -= int( $p->vrootx/2 ) if $p->vrootx;
+		$y -= int( $p->vrooty/2 ) if $p->vrooty;
+	}
+	$w->geometry( "+$x+$y" );
+
+	$w->deiconify();
+
+}
+
+sub yn_window {
+	my ($w, $message, $onclose, @args) = @_;
+
+	my $dialog = $w->Dialog(
+		-title => 'A Question',
+		-bitmap => 'question',
+		-text => $message,
+		-default_button => 'No',
+		-buttons => [ 'Yes', 'No' ],
+	);
+	my $ans = $dialog->Show();
+
+	&$onclose( @args ) if ref( $onclose ) eq 'CODE';
+
+	return $ans eq "Yes";
+}
+
+sub done_window {
+	my ($w, $message, $onclose, @args) = @_;
+
+	my $dialog = $w->Dialog(
+		-title => 'Done!',
+		-bitmap => 'info',
+		-text => $message,
+		-default_button => 'OK',
+		-buttons => [ 'OK' ],
+	);
+	$dialog->Show();
+
+	&$onclose( @args ) if ref( $onclose ) eq 'CODE';
+
+	1;
+}
+
+sub error_window {
+	my ($w, $message, $onclose, @args) = @_;
+
+	my $dialog = $w->Dialog(
+		-title => 'ERROR!',
+		-bitmap => 'error',
+		-text => $message,
+		-default_button => 'OK',
+		-buttons => [ 'OK' ],
+		);
+	$dialog->Subwidget( 'bitmap' )->configure( -foreground => 'red' );
+	$dialog->Subwidget( 'message' )->configure( -foreground => 'red' );
+	$dialog->Show();
+
+	&$onclose( @args ) if ref( $onclose ) eq 'CODE';
+
+	1;
+}
+
+sub init_si_config {
+	my $config = new AppConfig(
+		DEFAULT_IMAGE_DIR => { ARGCOUNT => 1},
+		AUTOINSTALL_SCRIPT_DIR => { ARGCOUNT => 1},
+		AUTOINSTALL_BOOT_DIR => { ARGCOUNT => 1},
+		RSYNCD_CONF => { ARGCOUNT => 1},
+		RSYNC_STUB_DIR => { ARGCOUNT => 1},
+		CONFIG_DIR => { ARGCOUNT => 1},
+		TFTP_DIR => { ARGCOUNT => 1},
+		NET_BOOT_DEFAULT => { ARGCOUNT => 1},
+		# now for tksis configuration parameters
+		ICON_DIR => { ARGCOUNT => 1, DEFAULT => "/usr/share/systeminstaller/images"},
+		XTERM_CMD => { ARGCOUNT => 1, 
+			DEFAULT => "xterm -bg black -fg magenta",
+		},
+	);
+	$config->file("/etc/systemimager/systemimager.conf", "/etc/systeminstaller/tksis.conf");
+	return $config;
+}
+
+#
+#  resethash sets one hash to another hash.
+#
+
+sub resethash {
+	my ($hash1, $hash2) = @_;
+	foreach my $key (keys %$hash2) {
+		$$hash1{$key} = $$hash2{$key};
+	}
+}
+
+#
+#  The following creates a set of widgets with do 'Label: [Entry]' 
+#
+
+sub label_entry_line {
+	my ($window, $labeltext, $variable, $validate, @morewidgets) = @_;
+	my @options;
+	if($validate) {
+		@options = (
+			-validatecommand => $validate,
+			-validate => "focusout",
+		);
+	}
+	my $label = $window->Label(-text => "$labeltext: ", -anchor => "w");
+	my $entry = $window->Entry(-textvariable => $variable, @options);
+	$label->grid($entry,@morewidgets, -sticky => "nesw");
+}
+
+# This creates a small list box with 1 item ($selection) selected.
+sub label_listbox_line {
+	my ($window, $labeltext, $selection, $listitems , @morewidgets) = @_;
+	my $label = $window->Label(-text => "$labeltext: ", -anchor => "w");
+	my $listbox = $window->Scrolled("Listbox",-scrollbars => 'e', -height => 2, 
+		-width => 17, -selectmode => "single", -exportselection=>0);
+	$listbox->insert(0,$selection);
+	$listbox->insert('end',@$listitems);
+	$listbox->selectionSet(0);
+
+	$label->grid($listbox,@morewidgets);
+	return $listbox;
+}
+
+sub label_entry_file_line {
+	
+}
+
+sub quit_button {
+	my $window = shift;
+	my $quit_button = $window->Button(
+		-text=>"Close",
+		-command=> [sub { shift->destroy }, $window],
+		-pady => 8,
+		-padx => 8,
+	);
+	return $quit_button;
+}
+
+sub label_option_line {
+	my ($window, $labeltext, $variable, $options, @morewidgets) = @_;
+	my $label = $window->Label(-text => "$labeltext: ", -anchor => "w");
+
+	my $default = $$variable;
+	my $optionmenu = $window->Optionmenu(-options => $options, -variable => $variable);
+	$optionmenu->setOption($default) if $default;
+
+	$label->grid($optionmenu, @morewidgets, -sticky => "nesw");
+	return $optionmenu;
+}
+
+sub imageexists {
+	my ($rsyncconf, $imagename) = @_;
+	open(IN,"<$rsyncconf") or return undef;
+	if(grep(/\[$imagename\]/, <IN>)) {
+		close(IN);
+		return 1;
+	}
+	close(IN);
+	return undef;
+}
+
+
+1;
---
+++ systeminstaller-oscar/tags/sin-oscar-2.2.0-1/lib/SystemInstaller/Tk/Help.pm
@@ -0,0 +1,81 @@
+package SystemInstaller::Tk::Help;
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+use base qw(Exporter);
+use SystemInstaller::Tk::Common;
+use Tk::ROText;
+use vars qw(@EXPORT %Help);
+use strict;
+
+@EXPORT = qw(%Help helpwindow helpbutton);
+
+%Help = (
+         "Image Name" => "Image Name:\n\nThe name you wish to call the image.",
+         "Package Directory" => "Package Directory:\n\nThis option specifies the directory which contains the packages (RPM, Deb, or other) which will be used to build the image.\n\nAll the packages used to build an image must be in the same directory.",
+         "Package File" => "Package File:\n\nThis option specifies a text file which contains a list of all the packages that should be installed.\nThere are various samples for several distributions under /usr/share/systeminstaller/distinfo",
+         "Disk File" => "Disk File:\n\nThis option specifies a text file which contains the disk partitiontion table for the image.\n\nFor more information on the format please see the mksidisk man page. There is a sample disktable in /usr/share/doc/systeminstaller-<version>/disktable.",
+         "Target Architecture" => "Target Architecture:\n\nThis option specifies what architecture the images should be built for.  The value will default to the current architecture.\n\nThere is some support for cross architecture image building, though it is mostly untested.",
+         "Post Install Action" => "Post Install Action:\n\nThis option specifies what the client should do after installation has completed.  The options are:\n\nbeep - Clients will beep incessantly after succussful completion of an autoinstall.\n\nreboot - Clients will reboot themselves after successful completion of an autoinstall.\n\nshutdown -- Clients will halt themselves after successful completion of an autoinstall.",
+         "Virtual Disk" => "Virtual Disk:\n\nSpecify a virtual disk device for the Iseries machine.  The specified device will have the image copied to it, and can serve as a master virtual disk, that can be replicated using CPYNWSSTG commands.  WARNING:The contents of the  device  specified here will be completely destroyed.",
+         "IP Method" => "IP Assignment Method:\n\nThis option specifies how ip addresses will be assigned to clients. The options are:\n\ndhcp -- A DHCP server will assign IP addresses dynamically to clients installed. They may be assigned a different address each time.\n\nstatic -- The IP address the client uses during autoinstall will be permanently assigned to that client.\n\nreplicant -- Don't mess with the network settings in this image.  I'm using it as a backup and quick restore mechanism for a single machine.",
+         "Multicast" => "Multicast:\n\nPlease refer to the \"OSCAR Installation Guide\", Multicast Installs with SystemImager, for more information.\n\nNOTE: Multicast support is experimental and highly dependent on your network.",
+         #### Now we get the Addclients Help
+         "Image Name Addclients" => "Image Name:\n\nThe name of the image that this client will be attached to.  This image must already exist on your image server.",
+         "Domain Name" => "Domain Name:\n\nThe network domain name you wish to be assigned to the client.",
+         "Base Name" => "Base Name:\n\nThe hostname stub you wish to use for the client.  This value will be prepended to the number of the client to determine the client's hostname.",
+         "Number of Hosts" => "Number of Hosts:\n\nThe number of clients you wish to be defined with this definition.",
+         "Starting Number" => "Starting Number:\n\nThe first number in the range of clients you wish to allocate.\n\nFor instance, if you created a definition with 'Base Name' = 'www', 'Number of Hosts' = '5', and 'Starting Number' = '5', you would get hosts www5, www6, www7, www8, and www9.",
+         "Padding" => "The number of digits to pad the node names with.\n\nFor example a value of 3 would create nodes with names like www001, www002, where a value of 0 would create nodes like www1, www2.",
+         "Starting IP" => "Starting IP:\n\nThe IP address in the range of IPs to allocate.\n\nAllocating a range of ips accross a subnet boundry is not currently supported.",
+         "Subnet Mask" => "Subnet Mask:\n\nThe Network Subnet Mask.",
+         "Default Gateway" => "Default Gateway:\n\nThe default route to send all packets.",
+         #### Update Client helps
+         "Client Names" => "Client Names:\n\nA comma delimited list of client names that you wish to update.",
+         "Image Name Updateclients" => "Image Name:\n\nThe image you wish the clients to use.",
+         "MAC Address" => "MAC Address:\n\nThe MAC address of the install adapter of the client.",
+         "IP Address" => "IP Address:\n\nThe IP address of the install adapter of the client.",
+         "Root password" => "Root password:\n\nThe root password to be set in the image. It must be specified twice to ensure that it is correct.",
+         
+        );
+
+# 
+#  open_help_window
+#
+
+sub helpbutton {
+    my ($window, $tag) = @_;
+    $window->Button(
+                    -text=>"Help",
+                    -takefocus => 0,
+                    -command=> [\&helpwindow, $window, $tag],
+                    -pady => 4,
+                    -padx => 8,
+                   );
+}
+
+sub helpwindow {
+    my $window = shift;
+    my $tag = shift;
+    my $helpwindow = $window->Toplevel();
+    $helpwindow->withdraw;
+    $helpwindow->title("Help About: $tag");
+    my $ro = $helpwindow->Message(-text => $Help{$tag});
+    $ro->pack(-fill => "both", -expand => 1);
+    quit_button($helpwindow)->pack(-fill => "x");
+    center_window( $helpwindow );
+}
+
+1;
