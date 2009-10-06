3a127d62dd43a978ba11ad3408b24e8baf74c90a
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2009-10-06 04:53:55 +0000

    [Torque OPKG] Tag the current version of the OPKG.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@8837 18f9d06c-6ae2-0310-8438-938a554feff9

---
+++ opkg/torque/tags/20091006/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the torque package
+#
+0 4	* * *	root	torque_maintenance
---
+++ opkg/torque/tags/20091006/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(torque):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="torque" command="/usr/bin/torque"
