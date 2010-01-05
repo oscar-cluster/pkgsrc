6d85820d573e8387600f009d860b4dbdddffe55f
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2010-01-05 03:48:01 +0000

    [Torque] Tag a new version.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@9185 18f9d06c-6ae2-0310-8438-938a554feff9

---
+++ opkg/torque/tags/20100104/debian/torque-default.ex
@@ -0,0 +1,10 @@
+# Defaults for torque initscript
+# sourced by /etc/init.d/torque
+# installed at /etc/default/torque by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
